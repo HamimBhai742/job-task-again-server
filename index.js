59dacb951f14ae0258cc2189804dde89dfa76828
HamimBhai742 <omagootruelove147@gmail.com>
2024-08-15 19:12:06 +0600

    Card compleytejen

--- index.js
+++ index.js
@@ -32,6 +32,11 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/products', async (req, res) => {
+            const result = await productCollection.find().toArray()
+            res.send(result)
+        })
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
