2b64880b1efe7a269fefad6ec46636ac31fd23e4
HamimBhai742 <omagootruelove147@gmail.com>
2024-08-17 09:49:34 +0600

    add server name

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Crazy Shop Server Is Running.......')
 })
 
 app.listen(port, () => {
